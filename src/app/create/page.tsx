4291426b67c97d2ec9812e49a5585a1fb28bafb6
kf2304072 <cometrue.6398@gmail.com>
2023-11-05 21:19:38 +0900

    改行修正

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -3,35 +3,45 @@ import { Box, Button, Flex, Radio, RadioGroup, Spacer, Stack, Text, Textarea } f
 const create = () =>{
   return (
       <Box as="main" w="1280px" mx="auto">
-        <Flex as="header" h='80px' bg="#68D391" alignItems="center" px={5}>
-          <Box w="127px" h="56px" display="flex" alignItems="center" justifyContent="center">
+        <Flex
+          as="header"
+          h='80px'
+          bg="#68D391"
+          alignItems="center"
+          px={5}>
+          <Box
+            w="127px"
+            h="56px"
+            display="flex"
+            alignItems="center"
+            justifyContent="center">
             <Text fontSize="48px" fontWeight="bold" marginLeft="150px">TODO</Text>
           </Box>
           <Spacer/>
           <Box marginRight="50px">
-            <Button 
-              w="120px" 
-              h="56px" 
-              bg="#D9D9D9" 
-              borderWidth="1px" 
-              borderRadius="10px" 
-              cursor="pointer" 
-              fontSize="24px" 
+            <Button
+              w="120px"
+              h="56px"
+              bg="#D9D9D9"
+              borderWidth="1px"
+              borderRadius="10px"
+              cursor="pointer"
+              fontSize="24px"
               fontWeight="bold">
               LOGOUT
             </Button>
           </Box>
         </Flex>
 
-        <Flex mt={4} ml="100px" >
+        <Flex mt={4} ml="100px">
           <Box ml="968px">
-            <Button 
-              w="112px" 
-              h="40px" 
-              bg="#68D391" 
-              borderRadius="20px" 
-              cursor="pointer" 
-              fontWeight="bold" 
+            <Button
+              w="112px"
+              h="40px"
+              bg="#68D391"
+              borderRadius="20px"
+              cursor="pointer"
+              fontWeight="bold"
               fontSize="18px">
               BACK
             </Button>
@@ -40,29 +50,29 @@ const create = () =>{
 
         <Box as="section" mt={2} ml="100px">
           <Text fontSize="24px" fontWeight="bold">TITLE</Text>
-          <Textarea 
-            w="1080px" 
-            h="72px" 
-            size="lg" 
-            fontSize="24px" 
-            fontWeight="bold" 
-            lineHeight="1.25" 
-            padding="6px" 
-            resize="none" 
+          <Textarea
+            w="1080px"
+            h="72px"
+            size="lg"
+            fontSize="24px"
+            fontWeight="bold"
+            lineHeight="1.25"
+            padding="6px"
+            resize="none"
             borderRadius="12px"/>
         </Box>
 
         <Box as="section" mt={6} ml="100px">
           <Text fontSize="24px" fontWeight="bold">DETAIL</Text>
-          <Textarea 
-            w="1080px" 
-            h="204px" 
-            size="lg" 
-            fontSize="24px" 
-            fontWeight="bold" 
-            lineHeight="1.25" 
-            padding="6px" 
-            resize="none" 
+          <Textarea
+            w="1080px"
+            h="204px"
+            size="lg"
+            fontSize="24px"
+            fontWeight="bold"
+            lineHeight="1.25"
+            padding="6px"
+            resize="none"
             borderRadius="12px"/>
         </Box>
 
@@ -78,14 +88,14 @@ const create = () =>{
         </Box>
         <Flex mt={4} ml="100px">
           <Box ml="968px">
-            <Button 
-              w="112px" 
-              h="40px" 
-              bg="#25855A" 
-              borderRadius="20px" 
-              cursor="pointer" 
-              fontWeight="bold" 
-              color="white" 
+            <Button
+              w="112px"
+              h="40px"
+              bg="#25855A"
+              borderRadius="20px"
+              cursor="pointer"
+              fontWeight="bold"
+              color="white"
               fontSize="18px">
               CREATE
             </Button>
