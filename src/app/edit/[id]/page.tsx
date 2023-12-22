3f1f1fef41ae8109b2f142b9224d1aa1dc1d4622
shtk0119 <shtk0119@icloud.com>
2023-12-22 13:32:44 +0900

    修正

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import { Box, Button, Flex, Spacer, Text, Textarea } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
@@ -15,12 +16,8 @@ type Task = {
 };
 
 const Edit = ({ params }: { params: { id: string } }) => {
-
   const [task, setTask] = useState<Task | null>(null);
-  // const [create, setCreate] =useState<string>("");
-
   const router = useRouter();
-
   const paramId = params.id;
 
   const formatDate = (date: Date): string => {
@@ -100,107 +97,110 @@ const Edit = ({ params }: { params: { id: string } }) => {
     }
   };
 
-  const edit = () => {
-    return (
-      <Box as="main" w="1280px" mx="auto">
-        <Flex as="header" h="80px" bg="#68D391" alignItems="center" px={5}>
-          <Box
-            w="127px"
-            h="56px"
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
+  return (
+    <Box as="main" w="1280px" mx="auto">
+      <Flex as="header" h="80px" bg="#68D391" alignItems="center" px={5}>
+        <Box
+          w="127px"
+          h="56px"
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+        >
+          <Text fontSize="48px" fontWeight="bold" marginLeft="150px">
+            TODO
+          </Text>
+        </Box>
+        <Spacer />
+        <Box marginRight="50px">
+          <Logout />
+        </Box>
+      </Flex >
+
+      <Flex mt={4} ml="100px">
+        <Box ml="968px">
+          <Button
+            w="112px"
+            h="40px"
+            bg="#68D391"
+            borderRadius="20px"
+            cursor="pointer"
+            fontWeight="bold"
+            fontSize="18px"
           >
-            <Text fontSize="48px" fontWeight="bold" marginLeft="150px">
-              TODO
-            </Text>
-          </Box>
-          <Spacer />
-          <Box marginRight="50px">
-            <Logout />
-          </Box>
-        </Flex >
+            Back
+          </Button>
+        </Box>
+      </Flex>
 
-        <Flex mt={4} ml="100px">
-          <Box ml="968px">
-            <Button
-              w="112px"
-              h="40px"
-              bg="#68D391"
-              borderRadius="20px"
-              cursor="pointer"
-              fontWeight="bold"
-              fontSize="18px"
-            >
-              Back
-            </Button>
+      <Box as="section" mt={2} ml="100px">
+        <Text fontSize="24px" fontWeight="bold">
+          TITLE
+        </Text>
+        <Textarea
+          w="1080px"
+          h="72px"
+          size="lg"
+          fontSize="24px"
+          fontWeight="bold"
+          lineHeight="1.25"
+          padding="6px"
+          resize="none"
+          borderRadius="12px"
+          value={task?.title}
+          onChange={(e) => handleChange(e, 'title')}
+        />
+      </Box>
+
+      <Box as="section" mt={6} ml="100px">
+        <Text fontSize="24px" fontWeight="bold">
+          DETAIL
+        </Text>
+        <Textarea
+          w="1080px"
+          h="204px"
+          size="lg"
+          fontSize="24px"
+          fontWeight="bold"
+          lineHeight="1.25"
+          padding="6px"
+          resize="none"
+          borderRadius="12px"
+          value={task?.detail}
+          onChange={(e) => handleChange(e, 'detail')}
+        />
+      </Box>
+
+      <Flex mt={4} ml="100px">
+        <Flex>
+          <Box mr={5}>
+            <Text fontWeight="bold">Create</Text>
+            <Text fontWeight="bold">{task?.created_at as string}</Text>
+          </Box>
+          <Box>
+            <Text fontWeight="bold">Update</Text>
+            <Text fontWeight="bold">{task?.updated_at as string}</Text>
           </Box>
         </Flex>
 
-        <Box as="section" mt={2} ml="100px">
-          <Text fontSize="24px" fontWeight="bold">
-            TITLE
-          </Text>
-          <Textarea
-            w="1080px"
-            h="72px"
-            size="lg"
-            fontSize="24px"
+        <Box ml="660px">
+          <Button
+            w="112px"
+            h="40px"
+            bg="#25855A"
+            borderRadius="20px"
+            cursor="pointer"
             fontWeight="bold"
-            lineHeight="1.25"
-            padding="6px"
-            resize="none"
-            borderRadius="12px"
-          />
+            color="white"
+            fontSize="18px"
+            onClick={handleUpdate}
+          >
+            UPDATE
+          </Button>
         </Box>
-
-        <Box as="section" mt={6} ml="100px">
-          <Text fontSize="24px" fontWeight="bold">
-            DETAIL
-          </Text>
-          <Textarea
-            w="1080px"
-            h="204px"
-            size="lg"
-            fontSize="24px"
-            fontWeight="bold"
-            lineHeight="1.25"
-            padding="6px"
-            resize="none"
-            borderRadius="12px"
-          />
-        </Box>
-
-        <Flex mt={4} ml="100px">
-          <Flex>
-            <Box mr={5}>
-              <Text fontWeight="bold">Create</Text>
-              <Text fontWeight="bold">2023-01-01 00:00</Text>
-            </Box>
-            <Box>
-              <Text fontWeight="bold">Update</Text>
-              <Text fontWeight="bold">2023-01-01 00:00</Text>
-            </Box>
-          </Flex>
-
-          <Box ml="660px">
-            <Button
-              w="112px"
-              h="40px"
-              bg="#25855A"
-              borderRadius="20px"
-              cursor="pointer"
-              fontWeight="bold"
-              color="white"
-              fontSize="18px"
-            >
-              UPDATE
-            </Button>
-          </Box>
-        </Flex>
-      </Box >
-    );
-  };
+      </Flex>
+    </Box >
+  )
 }
 
 export default Edit;
